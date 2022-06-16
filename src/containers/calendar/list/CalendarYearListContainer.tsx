b6f4c5c12749a3dc2f47219b6702f5a473358804
Bin_TestProject <bhw110@naver.com>
2022-06-16 14:39:48 +0900

    해당 년도 할일 모아보기 삭제 기능 추가

--- src/containers/calendar/list/CalendarYearListContainer.tsx
+++ src/containers/calendar/list/CalendarYearListContainer.tsx
@@ -1,8 +1,13 @@
-import { useEffect } from "react";
+import { MouseEvent, useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CalendarYearList from "../../../components/calendar/list/CalendarYearList";
 import { RootState } from "../../../modules";
-import { changeCalendarMonth, listCalendar } from "../../../modules/calendar";
+import { changeCalendar, changeCalendarMonth, changeSubWrite, changeWrite, listCalendar } from "../../../modules/calendar";
+import { deleteCalendar } from "../../../modules/view";
+
+function timeout(delay:number) { 
+    return new Promise( res => setTimeout(res, delay) ); 
+}
 
 const CalendarYearListContainer = () => {
     const dispatch = useDispatch();
@@ -13,15 +18,44 @@ const CalendarYearListContainer = () => {
         user: user.user,
         loading: loading['calendar/LIST_CALENDAR'],
     }));
-
-    const {viewYear, viewMonth} = form;
  
     useEffect(() => {
-        const startMonth = viewYear+".01", endMonth = viewYear+".12";
+        const startMonth = form.viewYear+".01", endMonth = form.viewYear+".12";
         const thisDates = {startMonth, endMonth};
         dispatch(listCalendar(thisDates)); //처음 입장 시 
         dispatch(changeCalendarMonth(thisDates));
-    },[viewYear, viewMonth, dispatch]);
+    },[dispatch, form.viewYear]);
+
+    
+    const onClick = useCallback(async (e:MouseEvent<Element>, id:string, startDay:string) => {
+        const eClassName = e.target as Element;
+        if(eClassName.className === 'delete') {
+            if (window.confirm("정말 삭제합니까?")) {
+                const DateArray = startDay.split('.');
+                /* 삭제 관련 미들웨어 통신 */
+                await dispatch(deleteCalendar(id));
+                await timeout(300); //0.3초 딜레이
+                const startMonth = form.viewYear+".01", endMonth = form.viewYear+".12";
+                await dispatch(listCalendar({startMonth, endMonth}));
+                /* 종료 */
+
+                /* 삭제 된 할 일의 시작날짜로 전역 상태 값을 변경 */
+                dispatch(changeCalendar({
+                    viewYear: DateArray[0],
+                    viewMonth: DateArray[1],
+                    viewDate: DateArray[2],
+                }));
+                dispatch(changeWrite({key: 'startDay', value: startDay}));
+                dispatch(changeWrite({key: 'endDay', value: startDay}));
+                dispatch(changeSubWrite({form: 'startDate', key: 'year', value: DateArray[0]}));
+                dispatch(changeSubWrite({form: 'startDate', key: 'month', value: DateArray[1]}));
+                dispatch(changeSubWrite({form: 'startDate', key: 'date', value: DateArray[2]}));
+                dispatch(changeSubWrite({form: 'endDate', key: 'year', value: DateArray[0]}));
+                dispatch(changeSubWrite({form: 'endDate', key: 'month', value: DateArray[1]}));
+                dispatch(changeSubWrite({form: 'endDate', key: 'date', value: DateArray[2]}));
+            }
+        }
+    },[dispatch, form.viewYear]);
 
     if(calendarList === null) return <></>;
 
@@ -30,6 +64,7 @@ const CalendarYearListContainer = () => {
                 calendarList={calendarList}
                 User={user}
                 error={error}
+                onClick={onClick}
             />
 };
 
