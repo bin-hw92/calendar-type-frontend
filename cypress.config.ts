43ec0ff61d8cb201907d3a71e6c1057370c07444
Bin_TestProject <45866008+bin-hw92@users.noreply.github.com>
2022-11-09 10:41:29 +0900

    actions 테스트

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,22 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
+
+require('dotenv').config()
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+  // Viewport settings overridden for component tests
+  component: {
+    devServer: {
+      bundler: "webpack",
+      framework: "create-react-app",
     },
-  }
-})
+    experimentalSingleTabRunMode: false,
+  },
+  e2e: {
+    baseUrl: 'http://localhost:3000/',
+    experimentalSessionAndOrigin: true,
+  },
+  viewportWidth: 1320,
+  viewportHeight: 820,
+  projectId: "bt9t7m",
+  // The rest of the Cypress config options go here...
+});
